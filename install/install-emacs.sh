2a14310c94c2acb5fbded96e6b38db725ea382c6
frangipane <morgengruss@gmail.com>
2016-02-01 13:53:03 -0800

    fix conditional

--- install/install-emacs.sh
+++ install/install-emacs.sh
@@ -8,14 +8,16 @@ if [ -z "$(apt-cache policy emacs24 | grep "Candidate" | grep "24.4")" ]; then
     echo "emacs 24.4 is not available in ubuntu repo."
     echo "download and build from source."
 
+    sudo apt-get build-dep emacs24
+    
     # install required dependencies
-    if [sudo apt-get build-dep emacs24]; then
+    if [ $? -eq 0]; then
 	echo "*****emacs24 dependencies built!*****"
 	#wget $EMACS_URL && tar -xf emacs-24.4.tar.* && cd emacs-24.4
 	#./configure && make && sudo make install
 	#rm emacs-24.4.tar.xz
+	cd $PWD
     fi
-    cd $PWD
 else
     echo "emacs 24.4 is available in ubuntu repo...\n installing with apt-get."
     sudo apt-get install emacs24
