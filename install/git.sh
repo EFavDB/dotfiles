2d766e013a9afb620947b7436fec755dfda0b73b
frangipane <morgengruss@gmail.com>
2016-02-02 22:27:51 -0800

    simplify check for installation

--- install/git.sh
+++ install/git.sh
@@ -1,4 +1,5 @@
 ## skip installation if /opt/autoenv already exists
-[ -d /opt/autoenv ] && return
+[ -d /opt/autoenv ] || git clone git://github.com/kennethreitz/autoenv.git /opt/autoenv
 
-git clone git://github.com/kennethreitz/autoenv.git /opt/autoenv
+
+##
