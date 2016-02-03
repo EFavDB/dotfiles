c61f18478df521bf75d967fad4c1dd1a0018ed2f
frangipane <morgengruss@gmail.com>
2016-02-02 22:47:46 -0800

    emacs

--- install/git.sh
+++ install/git.sh
@@ -1,5 +1,7 @@
-## skip installation if /opt/autoenv already exists
+## autoenv - automatically activate virtual environment when you
+## cd into a directory containing a .env file (with activation instructions)
 [ -d /opt/autoenv ] || git clone git://github.com/kennethreitz/autoenv.git /opt/autoenv
 
 
-##
+## emacs
+[ -f ~/.emacs.d/init.el ] || git clone git://github.com/frangipane/emacs.git ~/.emacs.d
