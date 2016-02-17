a7888830a6582de63b613749334d9ab62b1c9f06
frangipane <morgengruss@gmail.com>
2016-02-17 11:48:18 -0800

    update emacs configs

--- install/git.sh
+++ install/git.sh
@@ -5,3 +5,8 @@
 
 ## emacs
 [ -f ~/.emacs.d/init.el ] || git clone git://github.com/frangipane/emacs.git ~/.emacs.d
+
+EMACS_DIR="$HOME/.emacs.d"
+
+## update Emacs configs
+[ -d "$EMACS_DIR/.git" ] && git --work-tree="$EMACS_DIR" --git-dir="$EMACS_DIR/.git" pull origin master
